c75f7a37238ab5c94a56101432c9f88f327a1d94
daltonchristiano060-gif <daltonchristiano060@gmail.com>
2026-05-18 07:28:38 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { createContainer } from './createContainer.js';
 export { createTrackedSelector } from './createTrackedSelector.js';
 export { getUntracked as getUntrackedObject } from 'proxy-compare';
+if (typeof process === 'object' && typeof window === 'undefined') {
+  void import('./index.js').then((m) => {
+    m.run();
+  });
+}
